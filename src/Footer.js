8b24cb8ef701614b1e1bdef2f94579e3003ed507
Suchitta05 <suchitta.mu.66@ubu.ac.th>
2023-06-20 12:07:45 +0700

    create react app

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Footer() {
+    return (
+        <div className='MyFooter'>
+            <span>Copyright 2023 @ Suchitta Munphan</span>
+        </div>
+    );
+}
+
+export default Footer;
